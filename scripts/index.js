0f52daf2fb836a61eb398913f8974cf05df23015
Christian Rönnholm <christian@kajal.se>
2025-01-22 13:55:46 +0100

    renamed some variables and functions, refactured into even more functions, an init function, added icon-package

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,38 @@
-let i = 0;
-//Representera varje aktivitet som ett objekt egenskaper som namn och status.
-
+// Globals
 let _activities = [];
-const _isDevelopment = true;
-
-// Get the modal
 var modal = document.getElementById("myModal");
+let previousYESEventHandler;
 
-const cancelElements = [document.querySelector(".close"), document.querySelector("#modal-no"), document.querySelector("#modal-cancel")];
-// When the user clicks on element, close the modal
-const hideModal = () => {
-    modal.style.display = "none";
-}
-cancelElements.forEach(el => {
-    
-    if (el) {
-        el.addEventListener('click', () => { hideModal(); });
+function setupModalHide()
+{
+    modal.hide = () => {       
+        modal.style.display = "none";
     }
-});
+    const cancelElements = [document.querySelector(".close"), document.querySelector("#modal-no"), document.querySelector("#modal-cancel")];
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        hideModal();
+    cancelElements.forEach(el => {    
+        if (el) {
+            el.addEventListener('click', () => { modal.hide(); });
+        }
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.hide();
+        }
     }
 }
 
-document.getElementById('bucketForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const form = event.target;
-    if (form.reportValidity())   
-        form.reset();
-});
+function addFormSubmitEvent()
+{
+    document.getElementById('bucketForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const form = event.target;
+        if (form.reportValidity())   
+            form.reset();
+    });
+}
 
 function addBucketFormButtonClickHandler() {    
     const el = document.getElementById('bucketForm').querySelector('button[type="submit"]');
@@ -49,39 +50,36 @@ function addBtnClearClickHandler() {
     });
 }
 
-let previousEventHandler;
-
-function modalConfirm(title, okFunction) {
+function modalConfirm(title, YESFunction) {
     const modalTitleEl = document.getElementById('modal-title');
     modalTitleEl.innerHTML = title;
 
     const btnEl = document.getElementById('modal-button');
-    if(previousEventHandler)
-        btnEl.removeEventListener('click', previousEventHandler)
+
+    //If we have opened the modal and assigned a click event before to the YES-button. We must first remove it. Else there will be multiple click events if we add another.
+    if(previousYESEventHandler)
+        btnEl.removeEventListener('click', previousYESEventHandler)
 
     const eventHandler = (e) => {
-        okFunction();
-        hideModal();
+        YESFunction();
+        modal.hide();
     };
 
-    previousEventHandler = eventHandler;
+    previousYESEventHandler = eventHandler;
 
     btnEl.addEventListener('click', eventHandler);   
     modal.style.display = "block";
 }
-
 
 function clearLocalStorage() {
     localStorage.clear();
     _activities = [];
 }
 
-
-//Used by submitbutton on click
+// Used by submitbutton on click
 function addFromFormAndRefresh() {
     const aName = document.getElementById('activityName').value.trim();
     const aCat = document.getElementById("activityCategory").value;
-    //const aCat = selectEl.options[selectEl.selectedIndex].value;
 
     addActivity(createActivity(aName, aCat, false));
 
@@ -101,10 +99,6 @@ function addTestItems() {
     addActivity(createActivity('England', 'Resor', false));
     addActivity(createActivity('Lexicon frontend React', 'Lärande', false));
     addActivity(createActivity('Lexicon .NET påbyggnad', 'Lärande', true));
-}
-
-function getActivities() {
-    return _activities;
 }
 
 function createActivity(name, category, status) {
@@ -133,7 +127,7 @@ function createHTML(parent, category, filteredActivities) {
         const h2 = document.createElement('h2');
         h2.innerHTML = category;
         return h2;
-    })()); //Immediately invoke the function
+    })()); // Immediately invoke the function
 
     const ul = parent.appendChild(document.createElement('ul'));
     ul.classList.add('flow');
@@ -154,7 +148,8 @@ function createHTMLActivity(activity) {
     const pDel = document.createElement('input');
     pDel.setAttribute("type", "button");
     pDel.setAttribute("title", "Delete");
-    pDel.value = '🗙';
+    pDel.classList.add("material-symbols-outlined");
+    pDel.value = 'delete';
     pDel.addEventListener('click', () => {
         const modalOK = () => { removeActivity(activity.id); redrawItems() };
         modalConfirm(`Really delete activity? ${activity.name}`, modalOK);
@@ -162,7 +157,6 @@ function createHTMLActivity(activity) {
     
     const pEl = document.createElement('p');
     pEl.innerHTML = activity.name;
-    //pEl.innerHTML = `id: ${activity.id} ${activity.name}`;
     
     pLi.appendChild(pCb);        
     pLi.appendChild(pEl);
@@ -174,19 +168,19 @@ function createHTMLActivity(activity) {
 function removeActivity(id) {
     _activities.splice(_activities.findIndex(e => e.id === id), 1);
 
-    //Save changes in localstorage
+    // Save changes in localstorage
     setALocalStorage(_activities);
 }
 
 function updateActivity(id, status) {
-    //Update the array in place
+    // Update the array in place
     _activities.forEach((a, index) => {
         if (a.id === id) {
             _activities[index] = { ...a, status: status };
         }
     });
 
-    //Can't do this since using const:
+    // Keeping this code as a comment since it's another way of doing this I like. For learning purposes if I go back to rehearse:
     // _activities = _activities.map(a =>
     //     a.id === id
     //         ? { ...a, status: status }
@@ -200,45 +194,44 @@ function updateActivity(id, status) {
 function addActivity(activity) {
     _activities.push(activity);
 
-    //Save changes in localstorage
+    // Save changes in localstorage
     setALocalStorage(_activities);
-}
-
-//Main thread program
-addBucketFormButtonClickHandler();
-addBtnClearClickHandler();
-
-const parseActivityJSONAsActivity = function (jsonActivity)
-{
-    const newActivity = Object.create(activityPrototype);
-    Object.assign(newActivity, jsonActivity);
-    return newActivity;
-}
-
-if (localStorage.getItem('ActivitySave') == null) {
-    //If this is first time visiting this page we are here
-
-    if (_isDevelopment)
-    {
-        addTestItems(); //This one save to setALocalStorage anyway
-    }
-    else {
-        //This is probably just a empty array but futureproofing logic incase it isn't anyone, we don't want null there anymore
-        setALocalStorage(_activities);
-    }
-}
-else
-{
-    //We are a returning visitor and want to load
-
-    //Load/Parse _activities from localStorage
-    const jsonFromStorage = localStorage.getItem('ActivitySave');
-    const objFromJSON = JSON.parse(jsonFromStorage);
-    _activities = objFromJSON.map(parseActivityJSONAsActivity);
 }
 
 function setALocalStorage(activities) {
     localStorage.setItem('ActivitySave', JSON.stringify(activities));
 }
 
-redrawItems();
+// Main thread program
+init(true);
+function init(_isDevelopment) {
+    addBucketFormButtonClickHandler();
+    addBtnClearClickHandler();
+    setupModalHide();
+    addFormSubmitEvent();
+
+    const parseActivityJSONAsActivity = function (jsonActivity) {
+        const newActivity = Object.create(activityPrototype);
+        Object.assign(newActivity, jsonActivity);
+        return newActivity;
+    }
+
+    if (localStorage.getItem('ActivitySave') == null) {
+        //If this is first time visiting this page we are here
+
+        if (_isDevelopment) {
+            addTestItems(); //This one save to setALocalStorage anyway
+        }
+        else {
+            //This is probably just a empty array but futureproofing logic incase it isn't anyone, we don't want null there anymore
+            setALocalStorage(_activities);
+        }
+    }
+    else {
+        //We are a returning visitor and want to load (Load/Parse _activities from localStorage)
+        const jsonFromStorage = localStorage.getItem('ActivitySave');
+        const objFromJSON = JSON.parse(jsonFromStorage);
+        _activities = objFromJSON.map(parseActivityJSONAsActivity);
+    }
+    redrawItems();
+}
